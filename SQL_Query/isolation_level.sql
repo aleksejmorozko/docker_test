742a91e08447668b518858b81c82dc5ecd1903f0
Aleksej Morozko <aleksejmorozko@gmai.com>
2020-10-01 23:29:13 +0300

    commit_8

--- SQL_Query/isolation_level.sql
+++ SQL_Query/isolation_level.sql
@@ -1,6 +1,6 @@
 drop table if exists t;
 create table t(id serial, name text);
-insert into t (name) values ('dura'),('lex'),('sed'),('lexxx');
+insert into t (name) values ('aaa'),('bbb'),('ccc'),('ddd');
 
 begin;
 set transaction isolation level repeatable read;
@@ -8,9 +8,9 @@ set transaction isolation level read committed;
 set transaction isolation level read uncommitted;
 set transaction isolation level serializable;
 
-update t set name = 'lex1' where id = 1;
-update t set name = 'lex1' where id = 2;
-update t set name = 'lex1' where id = 3;
+update t set name = 'XX11' where id = 1;
+update t set name = 'YY11' where id = 2;
+update t set name = 'ZZ11' where id = 3;
 savepoint a0;
 rollback to savepoint a0;
 commit;
