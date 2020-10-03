e1f5c2b60eadccb0ae0035fb77e6dd11bf877f8e
Aleksej Morozko <aleksejmorozko@gmai.com>
2020-10-03 19:28:24 +0300

    commit_8_4

--- SQL_Query/isolation_level.sql
+++ SQL_Query/isolation_level.sql
@@ -1,6 +1,4 @@
-drop table if exists t;
-create table t(id serial, b int);
-insert into t (b) values (0),(0),(0),(0);
+drop table if exists t; create table t(id serial, b int); insert into t (b) values (0);
 
 begin;
 set transaction isolation level repeatable read;
@@ -8,10 +6,10 @@ set transaction isolation level read committed;
 set transaction isolation level read uncommitted;
 set transaction isolation level serializable;
 
-update t set b=b+1 where id = 1;
-update t set b=b+1 where id = 2;
-update t set b=b+1 where id = 3;
-savepoint a0;
+update t set b=b+1;
+
+savepoint a0; 
+savepoint a1;
 rollback to savepoint a0;
 commit;
 
@@ -21,8 +19,7 @@ select xmin, xmax, * from t;
 select txid_current();
 select txid_current_snapshot();
 
-select virtualxid, mode from pg_locks 
-where pid = pg_backend_pid() and locktype = 'virtualxid';
+select virtualxid, mode from pg_locks where pid = pg_backend_pid() and locktype = 'virtualxid';
 
 create extension if not exists pgrowlocks;
 select * from pgrowlocks('t');
