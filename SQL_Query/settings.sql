0ee8a6c55ecd14440793750a89fca5f2ddea4806
Aleksej Morozko <aleksejmorozko@gmai.com>
2020-10-07 23:51:55 +0300

    commit_9_3

--- SQL_Query/settings.sql
+++ SQL_Query/settings.sql
@@ -4,7 +4,7 @@ where sourcefile LIKE '%postgresql.conf';
 
 select name, setting, unit, boot_val,reset_val, source, sourcefile, sourceline, pending_restart, context
 from pg_settings
-where name = 'work_mem'; --\gx
+where name = 'work_mem'; --\gx --
 
 select pg_reload_conf();
 
@@ -16,10 +16,12 @@ show work_mem;
 alter system reset work_mem;
 select current_setting('work_mem');
 
-select set_config('work_mem', '32MB', false); --(true/false показывает на постояннку поменять или на сеанс)
+select set_config('work_mem', '32MB', false);           --(true/false показывает на постояннку поменять или на сеанс)
 
-select * from pg_settings;    --настройки работы БД
+select * from pg_settings;                              --настройки работы БД
 
 select * from pg_ls_waldir() order by name;
 
-create extension pgcrypto; select digest('Hello world', 'md5'); --создание исключения "extension" для шифрования строк. шифруется функцией "digest"
+create extension pgcrypto; 
+select digest('Hello world', 'md5');                    --создание исключения "extension" для шифрования строк. шифруется функцией "digest"
+select pg_size_pretty(pg_database_size('testdb'));      --размер БД. Функция "pg_database_size"
