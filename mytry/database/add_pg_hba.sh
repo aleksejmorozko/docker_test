08a5a132bb6487ded0ac52b70c0269e52592b208
Aleksej Morozko <aleksejmorozko@gmai.com>
2020-10-19 19:04:04 +0300

    commit_10_2

---
+++ mytry/database/add_pg_hba.sh
@@ -0,0 +1,2 @@
+echo 'host    replication     postgres    192.168.100.0/24    trust' >> pg_hba.conf
+echo 'host    all     all     192.168.100.0/24    trust' >> pg_hba.conf
--- mytry/database/fold.sh
+++
@@ -1,5 +0,0 @@
-#mkdir /var/lib/postgresql/data/archive && chmod 700 /var/lib/postgresql/data/archive && chown -R postgres:postgres /var/lib/postgresql/data/archive
-
-touch /var/log/postgresql.log
-chmod a-r,u+r /var/log/postgresql.log
-chown postgres:postgres /var/log/postgresql.log
