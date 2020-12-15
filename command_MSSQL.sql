98964af068bd402c1031c14964e7f36dd59dcb1a
AleksejMorozko <aleksejmorozko@gmail.com>
2020-12-15 12:47:12 +0300

    commit_12_18

--- command_MSSQL.sql
+++ command_MSSQL.sql
@@ -82,3 +82,57 @@ select * from HumanResources.Department
 SELECT TABLE_NAME AS [Название таблицы]
    FROM INFORMATION_SCHEMA.TABLES
    WHERE table_type='BASE TABLE'
+
+
+--------- Backup POWERSHELL ---------------------
+  Backup-SqlDatabase -ServerInstance localhost\
+    -Database AdventureWorks2017\
+    -BacupAction Log\
+    -BackupFile 'C:\Backup\awLogs.bak'
+
+--------- Backup ServerManagmentStudio --------------------
+  Backup LOG AdventureWorks2017
+    To Disk = N'C:\Backup\AdvWorks.bak'
+    with init;
+  Go
+  
+  Backup Database AdventureWorks2017
+    To Disk = N'C:\Backup\AdvWorks.bak'
+    with differential;                        --тип бэкапа
+  Go
+
+  -- Partial backup - нельзя сделать в SMS, только TransactSQL, 
+  Backup database LargeDB read_write_filegroups to DISK = 'R:\bacups\LrgRW.bak' WITH INIT;
+
+  --A FILE GROUP:`
+  Backup database LargeDB Filegroup='LrgFG' to DISK='R:\backups\LrgFG.bak' WITH INIT;
+
+  ------------------ Пример процесса восстановления БД ------------------------------- 
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_full.bak' WITH NORECOVERY, REPLACE
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_diff.bak' WITH NORECOVERY
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_log1.trn' WITH NORECOVERY
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_log2.trn' WITH NORECOVERY
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_log3.trn' WITH NORECOVERY
+RESTORE DATABASE work_demo FROM DISK='d:\bacups\work_demo_TailLog.trn' WITH NORECOVERY
+RESTORE DATABASE work_demo WITH RECOVERY
+
+  
+-------   Bulk Copy Program: bcp - выгрузка/загрузка в/из файл данных ----------------
+--Export
+bcp AdventureWorks2017.HumanResources.Department format nul -x -f d:\dept_format.xml -c -T
+bcp AdventureWorks2017.HumanResources.Department OUT d:\dept.bcp -T -c
+--Import
+bcp Resources.dbo.Depts IN D:\dept.bcp -f D:\dept_format.xml 
+
+BULK INSERT dbo.Depts
+  FROM 'd:\dept.bcp'
+  WITH (formatfile = 'd:\dept_format.xml')
+
+
+select * into dbo.Departments
+  FROM OPENROWSET (
+    BULK 'D:\depts.bcp',
+    FORMATFile = 'D:\depts_format.xml'
+  ) AS t1;
+GO
+
